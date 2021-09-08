758b298638a40b459c5e6ebaae78e6df95fef275
edkish <edkish@miners.utep.edu>
2021-09-08 11:44:19 -0600

    Added print arrow method

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,9 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints a arrow
+void print_arrow()
+{
+  print_triangle(3,5);
+  print_square(6,5);  
+}
--- uimain.c
+++ uimain.c
@@ -29,6 +29,10 @@ int main()
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       break;
+    case 'a':
+      puts("You selected arror");
+      print_arrow();
+      break;
     case 'q':
       puts("Bye!");
       goto done; 		/* terminate */
